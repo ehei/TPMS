b977dd8b2727b9dad721b481ed6921e1d2a3e4ad
ehei <ericheikkila@gmail.com>
2022-11-08 23:34:42 -0500

    back to fetching

--- tpm-app/src/App.js
+++ tpm-app/src/App.js
@@ -1,10 +1,10 @@
 import * as React from "react";
 import { Admin, Resource, ListGuesser } from 'react-admin';
-import {springDataProvider} from 'ra-data-spring-rest';
-import FetchClient from "./FetchClient";
+import {springDataProvider, springHttpClient} from 'ra-data-spring-rest';
+import httpClient from "./FetchClient";
+
 const dataProvider = springDataProvider('http://localhost:8080', httpClient);
 
-const httpClient = FetchClient();
 
 const App = () => (
     <Admin dataProvider={dataProvider}>
--- tpm-app/src/FetchClient.js
+++ tpm-app/src/FetchClient.js
@@ -1,16 +1,40 @@
 import {springHttpClient} from "ra-data-spring-rest";
-import fetch from 'node-fetch';
+import {fetchUtils} from "react-admin";
 
 const httpClient = springHttpClient(async (url, options = {}) => {
-    const [response] = await Promise.all([fetch(url, options)])
 
-    console.log(response.ok);
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.headers.raw());
-    console.log(response.headers.get('content-type'));
+    return fetchUtils.fetchJson(url, options)
+        .then(response => {
+            console.log(response.status);
+            console.log(response.body);
+            console.log(response.json);
 
-    return response;
+/*
+{
+  "_embedded" : {
+    "terms" : [ ]
+  },
+  "_links" : {
+    "self" : {
+      "href" : "http://localhost:8080/terms?page=0&size=10&sort=id%2CASC"
+    },
+    "profile" : {
+      "href" : "http://localhost:8080/profile/terms"
+    }
+  },
+  "page" : {
+    "size" : 10,
+    "totalElements" : 0,
+    "totalPages" : 0,
+    "number" : 0
+  }
+}
+ */
+            // is it worth trying to convert
+            // OR
+            // just add new endpoints that return the correct format?
+            // { data: {Record[]}, total: {int} }
+        })
 });
 
 export default httpClient;
