81766f06920ccce3cda9a6181d574702faf4f531
ehei <ericheikkila@gmail.com>
2022-11-15 22:47:10 -0500

    clean up unused user data store method

--- src/main/kotlin/com/ehei/tpms/server/datastore/UserRepository.kt
+++ src/main/kotlin/com/ehei/tpms/server/datastore/UserRepository.kt
@@ -2,13 +2,5 @@ package com.ehei.tpms.server.datastore
 
 import com.ehei.tpms.server.model.User
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
-import java.util.*
 
-interface UserRepository: JpaRepository<User, Long> {
-
-    @Query("select u from #{#entityName} u where u.username = :username")
-    fun findByUsername(@Param("username") username: String) : Optional<User>
-
-}
+interface UserRepository: JpaRepository<User, Long>
