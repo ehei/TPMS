e9b00fdcf38ace1b97fbcf2ba3d7d324647fa518
ehei <ericheikkila@gmail.com>
2022-11-15 23:20:45 -0500

    remove note and related classes

--- src/main/kotlin/com/ehei/tpms/server/datastore/NoteRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.ehei.tpms.server.datastore
-
-import com.ehei.tpms.server.model.Note
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface NoteRepository: JpaRepository<Note, Long>
