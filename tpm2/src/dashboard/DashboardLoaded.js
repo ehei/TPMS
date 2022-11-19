7b83db3455721eb2ec8b9e812f99b69cbf95ab94
ehei <ericheikkila@gmail.com>
2022-11-19 03:36:19 -0500

    adding nested info to dashboard

--- tpm2/src/dashboard/DashboardLoaded.js
+++ tpm2/src/dashboard/DashboardLoaded.js
@@ -29,6 +29,8 @@ const DashboardLoadedView = ({
         return (
         <div className={classes.root}>
             <Stack
+                marginTop={2}
+                paddingTop={2}
                 direction="column"
                 justifyContent="center"
                 alignItems="center"
@@ -42,18 +44,45 @@ const DashboardLoadedView = ({
                             <Card sx={{minWidth: 275, width: 500}} key={term.id}>
                                 <CardContent>
                                     <Typography variant="h2" color="secondary" gutterBottom>
-                                        {term.title}
+                                        <i>{term.title}</i>
                                     </Typography>
                                     <Typography variant="h5" component="div">
-                                        Start Date: {term.startDate}
-                                        <br/>
-                                        End Date: {term.endDate}
+                                        Start: {term.startDate}  End: {term.endDate}
+                                    </Typography>
+                                    <Typography variant="h5" component="div">
+                                        <br/><i>Courses</i><br/>
+                                        { term.courses.map(course => {
+                                            return (
+                                                <Typography variant="h6" component="div">
+                                                    <br/>
+                                                    {course.title}<br/>
+                                                    Start: {course.startDate} End: {course.endDate}
+                                                    <br/>&nbsp;&nbsp;&nbsp;<i>Assessments</i><br/>
+                                                    { course.assessments.map(assessment => {
+                                                        return (
+                                                            <Typography variant="h6" component="div">
+                                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{assessment.title}  ({assessment.performance? "Performance" : "Objective"})<br/>
+                                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Start: {assessment.startDate} End: {assessment.endDate}
+                                                                <br/>
+                                                            </Typography>);
+                                                    })}
+                                                    &nbsp;&nbsp;&nbsp;<i>Instructors</i><br/>
+                                                    { course.instructors.map(instructor => {
+                                                        return (
+                                                            <Typography variant="h6" component="div">
+                                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{instructor.name}<br/>
+                                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Phone: {instructor.phoneNumber} Email: {instructor.emailAddress}
+                                                                <br/>
+                                                            </Typography>);
+                                                    })}
+                                                </Typography>);
+                                        })}
                                     </Typography>
                                 </CardContent>
                             </Card>
                         );
                     })
-            }
+                }
             <div></div>
 
             </Stack>
--- tpm2/src/stories/testdata.js
+++ tpm2/src/stories/testdata.js
@@ -4,20 +4,113 @@ export const testdata = [
         "title": "term1",
         "startDate": "10/11/2022",
         "endDate": "12/12/2022",
-        "userId": "1"
+        "user": {
+            "id": 1,
+            "username": "someUser"
+        },
+        "courses": [{
+            "title": "A Title",
+            "startDate": "10/12/2022",
+            "endDate": "11/12/2022",
+            "instructors": [
+                {
+                    "name": "a name",
+                    "emailAddress": "e@mail.com",
+                    "phoneNumber": "1234567899"
+                }
+            ],
+            "assessments": [
+                {
+                    "title": "Assessment Uno",
+                    "startDate": "01/02/2022",
+                    "endDate": "02/03/2022",
+                    "performance": false
+                }
+            ]
+        }]
     },
     {
         "id": "2",
         "title": "term2",
         "startDate": "10/11/2022",
         "endDate": "12/12/2022",
-        "userId": "1"
+        "user": {
+            "id": 1,
+            "username": "someUser"
+        },
+        "courses": [
+            {
+                "id": 1,
+                "title": "A Title",
+                "startDate": "10/12/2022",
+                "endDate": "11/12/2022",
+                "instructors": [
+                    {
+                        "name": "a name",
+                        "emailAddress": "e@mail.com",
+                        "phoneNumber": "1234567899"
+                    }
+                ],
+                "assessments": [
+                    {
+                        "title": "Assessment Uno",
+                        "startDate": "01/02/2022",
+                        "endDate": "02/03/2022",
+                        "performance": false
+                    }
+                ]
+            },
+            {
+                "id": 2,
+                "title": "A Title",
+                "startDate": "10/12/2022",
+                "endDate": "11/12/2022",
+                "instructors": [
+                    {
+                        "name": "a name",
+                        "emailAddress": "e@mail.com",
+                        "phoneNumber": "1234567899"
+                    }
+                ],
+                "assessments": [
+                    {
+                        "title": "Assessment Uno",
+                        "startDate": "01/02/2022",
+                        "endDate": "02/03/2022",
+                        "performance": false
+                    }
+                ]
+            }
+            ]
     },
     {
         "id": "3",
         "title": "term3",
         "startDate": "10/11/2022",
         "endDate": "12/12/2022",
-        "userId": "1"
+        "user": {
+            "id": 1,
+            "username": "someUser"
+        },
+        "courses": [{
+            "title": "A Title",
+            "startDate": "10/12/2022",
+            "endDate": "11/12/2022",
+            "instructors": [
+                {
+                    "name": "a name",
+                    "emailAddress": "e@mail.com",
+                    "phoneNumber": "1234567899"
+                }
+            ],
+            "assessments": [
+                {
+                    "title": "Assessment Uno",
+                    "startDate": "01/02/2022",
+                    "endDate": "02/03/2022",
+                    "performance": false
+                }
+            ]
+        }]
     }
 ];
